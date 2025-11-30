262e2b05f1ccf7f5d5b6e91e217bfcce7ab5dff0
talya roth <talya21597@gmail.com>
2025-11-30 23:45:15 +0200

    Update SSL configuration for MySQL

--- server/server.js
+++ server/server.js
@@ -25,7 +25,9 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: 'Amazon RDS'
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.getConnection()
